fc9b22e2dfe02fcfb86f5ae172cb75605c0b296c
Scott Mahr <essmahr@gmail.com>
2016-10-16 16:30:53 +0200

    stuff

--- public/app/components/TapasList/TapasList.js
+++ public/app/components/TapasList/TapasList.js
@@ -28,7 +28,7 @@ class TapasList extends React.Component {
 
     return (
       <section styleName='list-container'>
-        <ListNav onNavClick={this.props.onNavClick} />
+        <ListNav onNavClick={this.props.onNavClick} styles={this.props.styles} />
         <header styleName='list-heading'>
           <h1 styleName='title'>{place.title}</h1>
           <h2 styleName='detail'>Visited <VisitCount count={visitCount}/></h2>
@@ -44,11 +44,26 @@ class TapasList extends React.Component {
 }
 
 function ListNav(props) {
+  const strokeWidth = 4;
+
   return (
-    <nav>
-      <button onClick={() => props.onNavClick('prev')}>prev</button>
-      <button onClick={() => props.onNavClick('next')}>next</button>
-      <button onClick={() => props.onNavClick('close')}>cose</button>
+    <nav className={props.styles['nav']}>
+      <button className={props.styles['prev']} onClick={() => props.onNavClick('prev')}>
+        <svg width="36px" height="36px" viewBox="0 0 36 36">
+          <polyline fill="none" points="26.5,1 9.5,18 26.5,35" stroke="#000000" strokeWidth={strokeWidth} strokeLinejoin="bevel" strokeMiterlimit="10"/>
+        </svg>
+      </button>
+      <button className={props.styles['next']} onClick={() => props.onNavClick('next')}>
+        <svg width="36px" height="36px" viewBox="0 0 36 36">
+          <polyline fill="none" points="9.5,35 26.5,18 9.5,1" stroke="#000000" strokeWidth={strokeWidth} strokeLinejoin="bevel" strokeMiterlimit="10"/>
+        </svg>
+      </button>
+      <button className={props.styles['close']} onClick={() => props.onNavClick('close')}>
+        <svg width="36px" height="36px" viewBox="0 0 36 36">
+          <line fill="none" stroke="#000000" strokeWidth={strokeWidth} strokeMiterlimit="10" x1="5" y1="5" x2="31" y2="31"/>
+          <line fill="none" stroke="#000000" strokeWidth={strokeWidth} strokeMiterlimit="10" x1="5" y1="31" x2="31" y2="5"/>
+        </svg>
+      </button>
     </nav>
   );
 }
