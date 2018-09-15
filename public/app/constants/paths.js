ddfc3b12f9761c78d87e8d362f3944ac6d25fa09
Scott Mahr <essmahr@gmail.com>
2018-09-15 12:50:35 -0700

    secure CDN URLs

--- public/app/constants/paths.js
+++ public/app/constants/paths.js
@@ -1,7 +1,6 @@
-const DB_JSON_URL = 'http://38cd67e50c7a1858bfa2-ad2aecdbb472ca5c311448ec1a75a30b.r27.cf1.rackcdn.com/tapas.json';
-const DB_IMAGES_DIR = 'http://38cd67e50c7a1858bfa2-ad2aecdbb472ca5c311448ec1a75a30b.r27.cf1.rackcdn.com/images/';
+const DB_JSON_URL =
+  "https://9000ef57835ed002c005-ad2aecdbb472ca5c311448ec1a75a30b.ssl.cf1.rackcdn.com/tapas.json";
+const DB_IMAGES_DIR =
+  "https://9000ef57835ed002c005-ad2aecdbb472ca5c311448ec1a75a30b.ssl.cf1.rackcdn.com/images/";
 
-export {
-  DB_JSON_URL,
-  DB_IMAGES_DIR,
-}
+export { DB_JSON_URL, DB_IMAGES_DIR };
